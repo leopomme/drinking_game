1dc6da3c3de475460a38c256efd9d1828a34c2e9
Leopold Stevens <lh5218@ic.ac.uk>
2024-07-07 17:44:29 +0200

    updates

--- app.py
+++ app.py
@@ -7,9 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/settings')
-def settings():
-    return render_template('settings.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
