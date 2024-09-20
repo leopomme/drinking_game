28ac10c1c5524d5be5ffea5599002e2823d7f40c
Leopold Stevens <lh5218@ic.ac.uk>
2024-09-20 18:52:51 +0200

    test static and web app

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-# app.py
-from flask import Flask, render_template
+# from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+# app = Flask(__name__, static_folder='static', template_folder='templates')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# # Route for the home page
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# # Route to serve the manifest
+# @app.route('/manifest.json')
+# def manifest():
+#     return send_from_directory('.', 'manifest.json')
+
+# # Route to serve the service worker
+# @app.route('/service-worker.js')
+# def service_worker():
+#     return send_from_directory('.', 'service-worker.js')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'drinking-game-cache-v1';
+const urlsToCache = [
+    './',
+    './templates/index.html',
+    './static/style.css',
+    './static/script.js',
+    './static/conditions.js',
+    './manifest.json',
+    // Add any other files you want to cache
+  ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
